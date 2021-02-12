dc534b1aee5d2f6618dabb63808d50e96502e4e5
Jonathan Percival <jonathan.i.percival@gmail.com>
2021-02-12 13:02:24 -0700

    Updated refresh

--- fhir3/_refresh.sh
+++ fhir3/_refresh.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #DO NOT EDIT WITH WINDOWS
 tooling_jar=tooling-1.3.1-SNAPSHOT-jar-with-dependencies.jar
-input_cache_path=./input-cache
-ig_resource_path=./input/connectathon.xml
+input_cache_path=$PWD/input-cache
+ig_path=./input/connectathon.xml
+resources_path=./input/resources
+root_dir=$PWD
 #measure_to_refresh_path=$PWD/input/resources/measure/measure-EXM104-8.2.000.json
 
 set -e
@@ -11,7 +13,7 @@ wget -q --spider tx.fhir.org
 
 if [ $? -eq 0 ]; then
 	echo "Online"
-	fsoption="-fs https://cqm-sandbox.alphora.com/cqf-ruler-r4/fhir/"
+	fsoption=""
 else
 	echo "Offline"
 	fsoption=""
@@ -21,13 +23,13 @@ echo "$fsoption"
 
 tooling=$input_cache_path/$tooling_jar
 if test -f "$tooling"; then
-	java -jar $tooling -RefreshIG -ini="$PWD"/ig.ini -t -d -p -v $fsoption
+	java -jar $tooling -RefreshIG -root-dir="$root_dir" -ig-path="$ig_path" -rp="$resources_path" -t -d -p $fsoption
         #java -jar $tooling -RefreshIG -ini="$PWD"/ig.ini -mtrp="$measure_to_refresh_path" -t -d -p -v $fsoption
 else
 	tooling=../$tooling_jar
 	echo $tooling
 	if test -f "$tooling"; then
-		java -jar $tooling -RefreshIG -ini=C%~dp0/ig.ini -t -d -p -v $fsoption
+		java -jar $tooling -RefreshIG -root-dir="$root_dir" -ig-path="$ig_path" -rp="$resources_path" -t -d -p $fsoption
                 #java -jar $tooling -RefreshIG -ini="$PWD"/ig.ini -mtrp="$measure_to_refresh_path" -t -d -p -v $fsoption
 	else
 		echo IG Refresh NOT FOUND in input-cache or parent folder.  Please run _updateRefreshIG.  Aborting...
